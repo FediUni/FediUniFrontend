09329c7563cb71f416e9649a7021a202cb5549b6
Jared Mullin <JaredMullin@protonmail.com>
2022-02-04 13:03:12 +0000

    Add Registration component and methods

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { CookieService } from 'ngx-cookie-service';
 import { tap } from "rxjs";
 import { environment } from "../environments/environment";
@@ -12,18 +12,32 @@ export class AuthenticationService {
   constructor(private http: HttpClient, private cookieService: CookieService) {
   }
 
+  register(username: string, password: string, name: string) {
+    let registerForm: FormData = new FormData();
+    registerForm.append("username", username);
+    registerForm.append("password", password);
+    registerForm.append("name", name);
+    return this.http.post<JWTResponse>(`${environment.apiUrl}/register`, registerForm).pipe(
+      tap({
+        next: (res) => this.handleJWT(res),
+      })
+    );
+  }
+
   login(username: string, password: string) {
     let loginForm: FormData = new FormData()
     loginForm.append("username", username)
     loginForm.append("password", password)
     return this.http.post<JWTResponse>(`${environment.apiUrl}/login`, loginForm).pipe(
       tap({
-        next: (res) => {
-          let expirationTime = Date.parse(res.expires)
-          this.cookieService.set("jwt", res.jwt, expirationTime, "/", environment.domain, true, 'Strict')
-        },
+        next: (res) => this.handleJWT(res),
       })
     );
+  }
+
+  handleJWT(res: JWTResponse) {
+    let expirationTime = Date.parse(res.expires)
+    this.cookieService.set("jwt", res.jwt, expirationTime, "/", environment.domain, true, 'Strict')
   }
 }
 
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,64 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  register: FormGroup;
+  registerErr: string
+
+  constructor(fb: FormBuilder, private router: Router, private auth: AuthenticationService) {
+    this.register = fb.group({
+      'username': ['', [Validators.required, Validators.maxLength(12)]],
+      'password': ['', [Validators.required, Validators.maxLength(25)]],
+      'name': ['', [Validators.maxLength(25)]],
+    });
+    this.registerErr = '';
+  }
+
+  ngOnInit(): void {
+  }
+
+  getUsernameErrorMessage() {
+    if (this.register.controls['username'].getError('required')) {
+      return 'Username is a required field';
+    }
+    if (this.register.controls['username'].getError('maxlength')) {
+      return 'Username cannot exceed 12 characters';
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.register.controls['password'].getError('required')) {
+      return 'Password is a required field';
+    }
+    if (this.register.controls['password'].getError('maxlength')) {
+      return 'Password cannot exceed 25 characters';
+    }
+    return '';
+  }
+
+  getNameErrorMessage() {
+    if (this.register.controls['name'].getError('maxlength')) {
+      return 'Name cannot exceed 25 characters';
+    }
+    return '';
+  }
+
+  onSubmit() {
+    let username = this.register.value['username'];
+    let password = this.register.value['password'];
+    let name = this.register.value['name'];
+    this.auth.register(username, password, name).subscribe({
+      error: (err: HttpErrorResponse) => this.registerErr = err.error,
+      complete: () => this.router.navigate(['/'])
+    })
+  }
+}
