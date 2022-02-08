8aeaebbf1bac19b3cdb698711730df915eb77aa7
Jared Mullin <JaredMullin@protonmail.com>
2022-02-08 10:38:28 +0000

    Adjust Timeline Component to use Note component

--- src/app/vocab/Note.ts
+++ src/app/vocab/Note.ts
@@ -1,1 +1,16 @@
-export class Note implements Object { }
+import { Object } from './Object';
+import { Image } from './Image';
+import { Link } from './Link';
+
+export class Note implements Object {
+  type: String = '';
+  id: String = '';
+  attributedTo: Object[] = [];
+  content: String = '';
+  name: String = '';
+  icon: Image[] = [];
+  image: Image[] = [];
+  inReplyTo: Object[] = []
+  published: Date = new Date();
+  url: Link[] = [];
+}
