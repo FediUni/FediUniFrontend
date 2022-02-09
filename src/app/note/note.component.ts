6ccf791791d1c18914fe2372e9fc9b56772da63d
Jared Mullin <JaredMullin@protonmail.com>
2022-02-09 13:06:47 +0000

    Check profile picture is defined before using

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Note } from '../vocab/Note';
 import { Object } from '../vocab/Object';
 import { Actor } from '../vocab/Actor';
+import { Link } from '../vocab/Link';
 
 @Component({
   selector: 'app-note',
@@ -11,11 +12,16 @@ import { Actor } from '../vocab/Actor';
 export class NoteComponent implements OnInit {
   @Input() note: Object = new Note();
   @Input() author: Actor[] = [];
+  profilePicture: Link[] = [];
 
   constructor() {
   }
 
   ngOnInit(): void {
+    let icon = this.note.icon;
+    if (this.note.icon !== undefined && this.note.icon.length > 0) {
+      this.profilePicture = this.note.icon[0].url
+    }
   }
 
 }
