f89a2678e3df7422b4c7490f1922fbfafb6b8aef
Jared Mullin <JaredMullin@protonmail.com>
2022-02-09 19:38:00 +0000

    Fix the profilePicture usage

--- src/app/vocab/Image.ts
+++ src/app/vocab/Image.ts
@@ -1,14 +1,4 @@
 import { Object } from './Object';
+import { Link } from './Link';
 
-export class Image implements Object {
-  type: string = '';
-  id: string = '';
-  attributedTo: Object[] = [];
-  content: string = '';
-  name: string = '';
-  icon: Image[] = [];
-  image: Image[] = [];
-  inReplyTo: Object[] = []
-  published: Date = new Date();
-  url: Link[] = [];
-}
+export interface Image extends Object { }
