727d87b2fc1616bbc564c92d53dafd92b2104ed1
Jared Mullin <JaredMullin@protonmail.com>
2022-02-13 23:43:20 +0000

    Define vocab for OrderedCollectionPage

--- src/app/vocab/Collection.ts
+++ src/app/vocab/Collection.ts
@@ -9,10 +9,38 @@ export interface Collection extends ActivityPubObject {
   items: ActivityPubObject[]
 }
 
+export interface CollectionPage extends Collection {
+  partOf?: Link;
+  next?: Link;
+  prev?: Link;
+}
+
 export class OrderedCollection extends Object {
   totalItems: number = 0;
   current: Link = new Link();
   first: Link = new Link();
   last: Link = new Link();
   orderedItems: ActivityPubObject[] = [];
+
+  constructor(o: any) {
+    super();
+    this.totalItems = o.totalItems;
+    this.current = o.current;
+    this.first = o.first;
+    this.last = o.last;
+    this.orderedItems = o.orderedItems;
+  }
+}
+
+export class OrderedCollectionPage extends OrderedCollection {
+  partOf?: Link;
+  next?: Link;
+  prev?: Link;
+
+  constructor(o: any) {
+    super(o);
+    this.partOf = o.partOf;
+    this.next = o.next;
+    this.prev = o.prev;
+  }
 }
