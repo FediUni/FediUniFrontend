ff475ea68d74a1981011485acec44b81d512dbc5
Jared Mullin <JaredMullin@protonmail.com>
2022-02-25 20:06:00 +0000

    Support Creating Notes

--- src/app/actor.service.ts
+++ src/app/actor.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ActorService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthenticationService) { }
 
   getActor(identifier: string) {
     return this.http.get(
       `${environment.apiUrl}/actor?identifier=${identifier}`
     );
+  }
+
+  getCurrentActor() {
+    let username = this.auth.getUsername();
+    return this.http.get(`${environment.apiUrl}/actor/${username}`);
   }
 }
