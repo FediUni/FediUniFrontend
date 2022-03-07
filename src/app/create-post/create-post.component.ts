427eb5ddb32c3454d51eedf454a8472c76f423c4
Jared Mullin <JaredMullin@protonmail.com>
2022-03-07 12:59:11 +0000

    Ensure content does not solely consist of whitespace

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { AuthenticationService } from '../authentication.service';
@@ -14,7 +14,7 @@ export class CreatePostComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private postService: PostService, private auth: AuthenticationService) {
     this.post = fb.group({
-      content: ['', Validators.maxLength(500)]
+      content: ['', { validators: [Validators.required, Validators.maxLength(500), Validators.pattern(/[\S]/)] }]
     })
   }
 
@@ -22,7 +22,11 @@ export class CreatePostComponent implements OnInit {
   }
 
   onSubmit() {
+    if (this.post.invalid) {
+      return
+    }
     let content: string = this.post.value['content'];
+    content = content.trim();
     let username = this.auth.getUsername();
     let userID = new URL(`${environment.apiUrl}/actor/${username}`);
     let to: string = "https://www.w3.org/ns/activitystreams#Public";
@@ -42,5 +46,19 @@ export class CreatePostComponent implements OnInit {
       newContent.push('<p>' + line + '</p>')
     });
     return newContent.join('\n')
+  }
+
+  getContentErrorMessage() {
+    let content = this.post.controls['content'];
+    if (content.getError('required')) {
+      return 'Cannot submit an empty post';
+    }
+    if (content.getError('maxLength')) {
+      return `${content.value.length} / 500`;
+    }
+    if (content.getError('pattern')) {
+      return "Cannot submit an empty post";
+    }
+    return '';
   }
 }
