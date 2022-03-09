7eacfc380481010b9642de68adee50fc7a0bfc41
Jared Mullin <JaredMullin@protonmail.com>
2022-03-09 13:20:20 +0000

    Add additional logging

--- src/app/focused-activity/focused-activity.component.ts
+++ src/app/focused-activity/focused-activity.component.ts
@@ -20,17 +20,20 @@ export class FocusedActivityComponent implements OnInit {
     this.route.data.subscribe((res) => {
       this.activity = new Activity(res['activity']);
       this.activityService.getActivityWithReplies(this.activity.id).subscribe((res) => {
+        console.log(`Initial Response = ${res}`)
         this.replies = [];
         let collection = new Collection(res?.object?.replies);
         let first = collection?.first;
         if (first === undefined) {
           return
         }
+        console.log(`First Page = ${first}`)
         first.items.map((o) => this.replies.push(o))
         let next = first.next;
         if (next === undefined) {
           return
         }
+        console.log(`Next Page = ${next}`)
         next.items.map((o) => this.replies.push(o))
         console.log(this.replies)
       })
