b65d706e5da093a6fb510a8739592ad799bb277d
Jared Mullin <JaredMullin@protonmail.com>
2022-03-19 14:07:08 +0000

    File upload event working

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -9,13 +9,16 @@ import { Actor } from "../vocab/Actor";
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  allowedTypes: String[] = ['image/jpeg', 'image/jpg', 'image/png'];
   update: FormGroup;
   actor: Actor | undefined;
+  filename: String = '';
 
   constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.route.data.subscribe((res) => {
       this.actor = new Actor(res['actor'])
       this.update = this.fb.group({
+        profilePicture: [''],
         displayName: [this.actor.name, [Validators.maxLength(20)]],
         summary: [this.actor.summary, [Validators.maxLength(200)]],
       });
@@ -23,6 +26,29 @@ export class SettingsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onFileChanged(event: any): void {
+    let files: File[] = event?.target?.files || [];
+    if (files?.length != 1) {
+      return
+    }
+    const file: File = files[0];
+    console.log(file);
+    if (file) {
+      let reader = new FileReader();
+      let fileType = file?.type?.toLowerCase();
+      if (!this.allowedTypes.includes(fileType)) {
+        return
+      }
+      this.filename = file.name;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.update.setValue({
+          profilePicture: reader.result,
+        });
+      };
+    }
   }
 
   getDisplayNameError(): string {
